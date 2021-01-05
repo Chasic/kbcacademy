f4c8b6bd00a6a2775d82bf9499c2f8ec7ecb866b
Jan Řasa <honza.rasa@seznam.cz>
2021-01-05 19:58:45 +0100

    Change component def & cleanup

--- src/component.py
+++ src/component.py
@@ -1,27 +1,82 @@
 import csv
-import json
+import logging
+import os
 from pathlib import Path
+import datetime
+from kbc.env_handler import KBCEnvHandler
 
-DATA_FOLDER = Path('/data')
+APP_VERSION = "0.0.1"
 
-SOURCE_FILE_PATH = DATA_FOLDER.joinpath('in/tables/input.csv')
-RESULT_FILE_PATH = DATA_FOLDER.joinpath('out/tables/output.csv')
+KEY_PRINT_ROWS = 'print_rows'
+MANDATORY_PARS = [KEY_PRINT_ROWS]
 
-config = json.load(open(DATA_FOLDER.joinpath('config.json')))
-PARAM_PRINT_LINES = config['parameters']['print_rows']
+class Component(KBCEnvHandler):
 
-print('Running...')
-with open(SOURCE_FILE_PATH, 'r') as input, open(RESULT_FILE_PATH, 'w+', newline='') as out:
-    reader = csv.DictReader(input)
-    new_columns = reader.fieldnames
-    # append row number col
-    new_columns.append('row_number')
-    writer = csv.DictWriter(out, fieldnames=new_columns, lineterminator='\n', delimiter=',')
-    writer.writeheader()
-    for index, l in enumerate(reader):
-        # print line
-        if PARAM_PRINT_LINES:
-            print(f'Printing line {index}: {l}')
-        # add row number
-        l['row_number'] = index
-        writer.writerow(l)
+    def __init__(self, debug=False):
+        default_data_dir = Path(__file__).resolve().parent.parent.joinpath('data').as_posix() \
+            if not os.environ.get('KBC_DATADIR') else None
+
+        KBCEnvHandler.__init__(self, MANDATORY_PARS, log_level=logging.DEBUG if debug else logging.INFO,
+                               data_path=default_data_dir)
+
+        logging.info('Running version %s', APP_VERSION)
+        logging.info('Loading configuration...')
+
+        try:
+            self.validate_config(MANDATORY_PARS)
+        except ValueError as e:
+            logging.exception(e)
+            exit(1)
+
+    def run(self):
+        '''
+        Main execution code
+        '''
+        params = self.cfg_params
+        print_rows = params.get(KEY_PRINT_ROWS)
+
+        input_tables = self.get_input_tables_definitions()
+        first_input_table = input_tables[0]
+
+        print('Running...')
+
+        output_file_name = 'output.csv'
+        output_file = os.path.join(self.tables_out_path, output_file_name)
+
+        with open(first_input_table.full_path, 'r') as input, open(output_file, 'w+', newline='') as out:
+            reader = csv.DictReader(input)
+            new_columns = reader.fieldnames
+            # append row number col
+            new_columns.append('row_number')
+            writer = csv.DictWriter(out, fieldnames=new_columns, lineterminator='\n', delimiter=',')
+            writer.writeheader()
+            for index, l in enumerate(reader):
+                # print line
+                if print_rows:
+                    print(f'Printing line {index}: {l}')
+                # add row number
+                l['row_number'] = index
+                writer.writerow(l)
+
+        self.configuration.write_table_manifest(
+            file_name=output_file,
+            primary_key=['row_number'],
+            incremental=True
+        )
+
+        state = self.get_state_file()
+
+        last_update = None
+
+        if 'last_update' in state:
+            last_update = state['last_update']
+
+        print(f"Last update: {last_update}")
+
+        state['last_update'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        self.write_state_file(state)
+
+
+
+(Component()).run()
